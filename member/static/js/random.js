dc6270c0e1e61ba1ee05a6a72f002fabc2601e60
kebin0613 <realdevelope@naver.com>
2021-10-10 22:40:54 +0900

    feat : 랜덤비밀번호 생성기능 추가

--- member/static/js/random.js
+++ member/static/js/random.js
@@ -1,21 +1,9 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.3 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-
-//난수생성기 js로 하면 이렇게 간단한데ㅠㅠ
-// const randomString = Math.random().toString(36).slice(2);
-
-// console.log(randomString);
-    function randomPassword() {
-        var length = 8,
-            charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*|",
-            password = "";
-        for (var i = 0, n = charset.length; i < length; ++i) {
-            password += charset.charAt(Math.floor(Math.random() * n));
-        }
-        return password;
+function randomPassword() {
+    var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*|",
+        password = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
     }
+    return password;
+}
